d26cb14824ee84508188e4f9a4d396bf202cc71f
Tristan Dubbeld <tristan.dubbeld@gmail.com>
2025-02-18 17:30:55 +0100

    Add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { Home } from './pages/Home'
 import './App.css'
+import { BrowserRouter } from "react-router";
 
 function App() {
   return (
-    <Home />
+    <BrowserRouter>
+      <Home />
+    </BrowserRouter>
   )
 }
 
